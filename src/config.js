591d17435562c7d4b45aaef3b966df575bcbdd9a
yuku-huang <159754049+yuku-huang@users.noreply.github.com>
2024-02-14 09:14:59 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
     'whiteboard_id': 'd63aeb7e4704beae2c3f8f343b7fff491d76d41ee3199c78d4c4115b70c5f83b',
     'title': '羽空的卡片盒', // 站点标题
     'pages': {
-        'Articles': '75b7d7e0-faf3-44f9-96ec-518a0001ccd7',
-        'Projects': 'ca82336a-6341-465d-a303-869b760a43b2',
+        '部落格': '75b7d7e0-faf3-44f9-96ec-518a0001ccd7',
+        '使用方法': 'ca82336a-6341-465d-a303-869b760a43b2',
         'Activity': 'activity', // 花园活跃状态页面
-        'About': 'd47a1427-3c90-4ce2-9bf3-b7fab60cb05e'
+        '關於羽空的卡片盒': 'd47a1427-3c90-4ce2-9bf3-b7fab60cb05e'
     }
 }
 
