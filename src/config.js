1405cc9ae5ae183bb65b5ff77a4b64593f603427
yuku_huang <kelly58516776@gmail.com>
2026-02-19 00:43:33 +0800

    Update whiteboard_id in config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     'api_url': '', // 废弃
     'ga': 'G-XXXXXX',
-    'whiteboard_id': 'd63aeb7e4704beae2c3f8f343b7fff491d76d41ee3199c78d4c4115b70c5f83b',
+    'whiteboard_id': '946f23a3-75ef-48e3-8e7b-35c2376f2559',
     'title': '羽空的卡片盒', // 站点标题
     'pages': {
         '部落格': '75b7d7e0-faf3-44f9-96ec-518a0001ccd7',
